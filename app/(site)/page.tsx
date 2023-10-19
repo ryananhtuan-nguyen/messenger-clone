32464f9c62948c6da9468b7d059fe00d7c3e174b
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-19 13:23:33 +1300

    index page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+    </div>
+  )
+}
+
+export default Home
