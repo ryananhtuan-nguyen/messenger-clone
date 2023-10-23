5f05738c096304d519dc21fac9d5f3ed676e4da6
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-23 14:32:31 +1300

    button components

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Button from '@/app/components/Button'
 import Input from '@/app/components/inputs/Input'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
@@ -66,7 +67,22 @@ const AuthForm = () => {
             register={register}
             errors={errors}
           />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500"></span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
