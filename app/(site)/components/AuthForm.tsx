c3ef571fe0a41c58b5ae0671a1f505679d837e9c
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-23 15:22:53 +1300

    social buttons

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,9 +1,10 @@
 'use client'
-
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 import Button from '@/app/components/Button'
 import Input from '@/app/components/inputs/Input'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import AuthSocialButton from './AuthSocialButton'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -79,8 +80,29 @@ const AuthForm = () => {
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500"></span>
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
             </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === 'LOGIN'
+              ? 'New to Messenger?'
+              : 'Already have an account?'}
           </div>
         </div>
       </div>
