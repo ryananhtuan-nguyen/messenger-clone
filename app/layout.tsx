9dc2727d3e7209c8b718e9712090db102cdf97e8
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-26 15:13:06 +1300

    wrapped

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ToasterContext />
-        {children}
+        <AuthContext>{children}</AuthContext>
       </body>
     </html>
   )
