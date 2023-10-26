043ad6fc22c66332a12ee0de881a6480c3b330e7
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-26 16:22:20 +1300

    mobile items

---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import clsx from 'clsx'
+import Link from 'next/link'
+
+interface MobileItemProps {
+  // label: string
+  icon: any
+  href: string
+  onClick?: () => void
+  active?: boolean
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({
+  icon: Icon,
+  href,
+  onClick,
+  active,
+}) => {
+  const handleClick = () => {
+    if (onClick) return onClick()
+  }
+  return (
+    <Link
+      href={href}
+      onClick={onClick}
+      className={clsx(
+        `
+        group
+        flex
+        gap-x-3
+        text-sm
+        leading-6
+        font-semibold
+        w-full
+        justify-center
+        p-4
+        text-gray-500
+        hover:text-black
+        hover:bg-gray-100
+      `,
+        active && 'bg-gray-100 text-black'
+      )}
+    >
+      <Icon className="h-6 w-6" />
+    </Link>
+  )
+}
+
+export default MobileItem
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DesktopSidebar from './DesktopSidebar'
+import MobileFooter from './MobileFooter'
 
 const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   )
