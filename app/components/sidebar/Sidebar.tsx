cb4092d6c540136af1ed7b49514f84be161bb581
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-26 15:34:15 +1300

    desktop sidebar

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <DesktopSidebar />
+      <main className="lg:pl-20 h-full">{children}</main>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ app/users/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../components/sidebar/Sidebar'
+
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Sidebar>
+      <div className="h-full">{children}</div>
+    </Sidebar>
+  )
+}
+
+export default layout
