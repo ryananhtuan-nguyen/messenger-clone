1a628c7400867e116d475f78f3e0418aa19e45cd
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-28 23:47:32 +1300

    useConversations

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -2,15 +2,19 @@ import { useParams } from 'next/navigation'
 import { useMemo } from 'react'
 
 const useConversation = () => {
+  //Get params conversationId from URL
   const params = useParams()
+
+  //using useMemo to prevent rerendering as long as conversationId remains unchanged
   const conversationId = useMemo(() => {
     if (!params?.conversationId) {
       return ''
     }
   }, [params?.conversationId])
-
+  //variable to control the opening conversation
   const isOpen = useMemo(() => !!conversationId, [conversationId])
 
+  //return custom hook with useMemo
   return useMemo(
     () => ({
       isOpen,
