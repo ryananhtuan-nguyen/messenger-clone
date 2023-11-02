54e092cca4a8c8fc6dc3d222f60a92497080ad9d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-02 13:05:56 +1300

    get all users to display

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
+import getUsers from '../actions/getUsers'
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers()
+
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={user} />
+        {children}
+      </div>
     </Sidebar>
   )
 }
