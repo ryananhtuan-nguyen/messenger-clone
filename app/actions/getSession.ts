89de139a06b709d120fbbda9af585ba7b494651a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 11:31:35 +1300

    getsession

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth'
 
 import { authOptions } from '../api/auth/[...nextauth]/route'
-
+/* ---------------------Get current Session------------------------ */
 export default async function getSession() {
   return await getServerSession(authOptions)
 }
