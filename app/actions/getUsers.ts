de52c8b05cc18a517af1565a49af2d508c9ee7c6
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 11:33:32 +1300

    get current user

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -3,13 +3,17 @@ import prisma from '@/app/libs/prismadb'
 import getSession from './getSession'
 
 const getUsers = async () => {
+  //get current session
   const session = await getSession()
-
+  //if no session return [] as empty user []
   if (!session?.user?.email) {
     return []
   }
 
   try {
+    //find users to display in conversations
+    //exclude the current user, as current user's name
+    //does not need to be shown in the Convo box
     const users = await prisma.user.findMany({
       orderBy: {
         createdAt: 'desc',
@@ -20,7 +24,7 @@ const getUsers = async () => {
         },
       },
     })
-
+    //return users[]
     return users
   } catch (error: any) {
     return []
