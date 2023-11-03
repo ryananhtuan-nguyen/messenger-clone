e5810da5c7cdd0bbdbd9740ab39b07323d8e9758
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 11:30:48 +1300

    current user

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -4,22 +4,24 @@ import getSession from './getSession'
 
 const getCurrentUser = async () => {
   try {
+    //get current seesion
     const session = await getSession()
-
+    //if current user does not exist in database
     if (!session?.user?.email) {
       return null
     }
 
+    //find unique current user
     const currentUser = await prisma.user.findUnique({
       where: {
         email: session.user.email as string,
       },
     })
-
+    //if theres no current user found return null
     if (!currentUser) {
       return null
     }
-
+    //otherwise return current user
     return currentUser
   } catch (error: any) {
     return null
