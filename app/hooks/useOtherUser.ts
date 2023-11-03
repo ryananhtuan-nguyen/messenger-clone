8fd364a55f08e8e4c554bf80cbd1c501c2d68b3e
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 12:02:18 +1300

    use other hook

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -10,18 +10,21 @@ const useOtherUser = (
         users: User[]
       }
 ) => {
+  //get current session
   const session = useSession()
 
+  //get other users, to exclude current users
   const otherUser = useMemo(() => {
     const currentUserEmail = session?.data?.user?.email
 
     const otherUser = conversation.users.filter(
       (user) => user.email !== currentUserEmail
     )
-
+    //return the other user []
     return otherUser
   }, [session?.data?.user?.email, conversation.users])
 
+  //since this is a 1 on 1 convo, return the first item in the array
   return otherUser[0]
 }
 
