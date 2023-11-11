6fdcc09ac592127b6787df036547b80955667378
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-11 19:54:14 +1300

    fixed env

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -3,7 +3,7 @@ import PusherClient from 'pusher-js'
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: 'ap4',
   useTLS: true,
