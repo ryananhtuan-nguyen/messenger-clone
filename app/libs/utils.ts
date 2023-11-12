61cc9a366546f76c41322b9dee345ad16a8e373a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-12 14:09:42 +1300

    new metadata construction

---
+++ app/libs/utils.ts
@@ -0,0 +1,36 @@
+import { Metadata } from 'next'
+export function constructMetadata({
+  title = 'MessengerClone - a simple realtime messaging app',
+  description = 'MessengerClone is a chat app inspired by Facebook messenger, created just for learning purposes',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    icons,
+    metadataBase: new URL('https://messenger-clone-eight-navy.vercel.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
